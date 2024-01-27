93785823647e7774541df022fc45d423635f42b5
Alejandro Penagos <alejandropenagos1998@gmail.com>
2024-01-27 01:16:56 -0500

    update settings and api urls

--- ZendataAPI/ZendataAPI/urls.py
+++ ZendataAPI/ZendataAPI/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
+from django.views.generic import RedirectView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView, TokenVerifyView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='products/', permanent=True)),
+    path('products/', include('ProductApi.urls')),
+
+    # JWT
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
